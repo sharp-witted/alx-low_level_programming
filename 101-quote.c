74ac0070e7f8bbaad805cdcc408f01e689b6d290
Nancy Okayana <okayanana04@gmail.com>
2023-08-10 21:51:22 -0700

    0x00-hello_world

---
+++ 101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+*main - print without using printf and puts 
+*
+*Return: always(1)
+*/
+int main(void)
+{
+	fputs("and that piece of art is useful\", Dora Korpar, -2015-10-19\n", stdout);
+Return: (1);
+}
